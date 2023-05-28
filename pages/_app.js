935c8d28337a6bf1baf7ba093458217ec0fc6937
Rohit Kumar <55583961+RohitKumar-200@users.noreply.github.com>
2023-05-28 00:39:46 +0000

    Added gtag.js script

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,23 @@
 import { useEffect } from 'react';
 import { nanoid } from 'nanoid';
-import '@/styles/globals.css';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+import * as gtag from '@/public/scripts/gtag.js';
 import Layout from '../components/Layout';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   useEffect(() => {
     if (!sessionStorage.getItem('ip')) {
       fetch('https://api.ipify.org/?format=json')
@@ -21,8 +35,28 @@ export default function App({ Component, pageProps }) {
   }, []);
 
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
